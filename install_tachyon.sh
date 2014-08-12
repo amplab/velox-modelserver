c0d968347bcad26e40ff30e9a122c9464dd97bbf
Dan Crankshaw <dscrankshaw@gmail.com>
2014-08-12 23:36:40 +0000

    Added script to automatically clone tachyon and install to local maven repo.

---
+++ install_tachyon.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+TACHYON_HOME=~/tachyon
+
+if [ ! -d "$TACHYON_HOME" ]; then
+  git clone https://github.com/amplab/tachyon.git $TACHYON_HOME
+fi
+cd $TACHYON_HOME
+# check out a known working version of tachyon
+# this SHA is head of branch nrr2 as of 8/12/14
+git show-ref --verify --quiet refs/heads/<branch-name>
+# $? == 0 means local branch with <branch-name> exists. 
+git checkout 662f3db54ab08c7ff5f9bbcdf0431d7704929783
+mvn package -DskipTests
+mvn install:install-file -Dfile=core/target/tachyon-0.6.0-SNAPSHOT-jar-with-dependencies.jar -DgroupId=org.tachyonproject -DartifactId=tachyon-parent -Dversion=0.6.0-SNAPSHOT -Dpackaging=jar
+cd -
