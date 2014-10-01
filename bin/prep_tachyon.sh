54295b2241ff9b28f66224b3a2e9b4d4c4addb72
Dan Crankshaw <dscrankshaw@gmail.com>
2014-10-01 16:20:43 +0000

    Updating ec2 url

--- bin/prep_tachyon.sh
+++ bin/prep_tachyon.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env sh
 
 
+# source ~/ec2-variables.sh
+
 curl -H "Content-Type: application/json" -d '{
-  "itemsDst":"tachyon://ec2-54-224-72-176.compute-1.amazonaws.com:19998/item-model",
-"usersDst":"tachyon://ec2-54-224-72-176.compute-1.amazonaws.com:19998/user-model",
-"obsDst":"tachyon://ec2-54-224-72-176.compute-1.amazonaws.com:19998/movie-ratings",
+  "itemsDst":"tachyon://ec2-54-242-127-97.compute-1.amazonaws.com:19998/item-model",
+"usersDst":"tachyon://ec2-54-242-127-97.compute-1.amazonaws.com:19998/user-model",
+"obsDst":"tachyon://ec2-54-242-127-97.compute-1.amazonaws.com:19998/movie-ratings",
 "itemsSrc":"/home/ubuntu/data/product_model.txt",
 "usersSrc":"/home/ubuntu/data/user_model.txt",
 "obsSrc":"/home/ubuntu/data/ratings.dat",
@@ -12,7 +14,7 @@ curl -H "Content-Type: application/json" -d '{
 
 
 # curl -H "Content-Type: application/json" -d '{
-#   "tachloc":"tachyon://ec2-54-224-72-176.compute-1.amazonaws.com:19998/test-store",
+#   "tachloc":"tachyon://ec2-54-242-127-97.compute-1.amazonaws.com:19998/test-store",
 #   "create":true,
 #   "part":0,
 #   "key":123}' http://localhost:8080/misc/prep-tachyon
