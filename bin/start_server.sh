0a4d0ed8566773ed66e7d5ec91cbd3d0e6629b79
Dan Crankshaw <dscrankshaw@gmail.com>
2014-10-03 01:44:30 +0000

    Updated start script to scala 2.10

--- bin/start_server.sh
+++ bin/start_server.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 java -Xms5g -Xmx10g -Dlog4j.configuration=file:conf/log4j.properties \
-  -cp assembly/target/scala-2.11/velox-assembly-0.1.jar edu.berkeley.veloxms.VeloxApplication server conf/config.yml
+  -cp assembly/target/scala-2.10/velox-assembly-0.1.jar edu.berkeley.veloxms.VeloxApplication server conf/config.yml
