75b7e15ff9bd895027f8da2f157ce3206521d626
Tomer Kaftan <tomer.kaftan@gmail.com>
2014-10-23 12:36:35 -0700

    Made the storages nicely configurable through the yaml file

--- veloxms-core/src/main/scala/edu/berkeley/veloxms/storage/CassandraStorage.scala
+++ veloxms-core/src/main/scala/edu/berkeley/veloxms/storage/CassandraStorage.scala
@@ -14,30 +14,32 @@ import edu.berkeley.veloxms.util.KryoThreadLocal
 // class CassandraStorage[U: ClassTag] (
 class CassandraStorage ( address: String,
                          keyspace: String,
-                         usersCF: String,
-                         itemsCF: String,
-                         ratingsCF: String,
+                         users: String,
+                         items: String,
+                         ratings: String,
                          val numFactors: Int) extends ModelStorage[FeatureVector] with Logging {
 
   // Set up the Cassandra cluster and session
   private val cluster = Cluster.builder().addContactPoint(address).build()
-  private val session = cluster.connect(keyspace)
+  private val session = cluster.connect()
 
   // Constants for column names
   private val Key = "key"
   private val Value = "value"
 
   // Make sure all tables exist and contain the necessary columns
-  require(session.execute("SELECT * FROM " + usersCF + " LIMIT 1").getColumnDefinitions.contains(Key))
-  require(session.execute("SELECT * FROM " + usersCF + " LIMIT 1").getColumnDefinitions.contains(Value))
-  require(session.execute("SELECT * FROM " + itemsCF + " LIMIT 1").getColumnDefinitions.contains(Key))
-  require(session.execute("SELECT * FROM " + itemsCF + " LIMIT 1").getColumnDefinitions.contains(Value))
-  require(session.execute("SELECT * FROM " + ratingsCF + " LIMIT 1").getColumnDefinitions.contains(Key))
-  require(session.execute("SELECT * FROM " + ratingsCF + " LIMIT 1").getColumnDefinitions.contains(Value))
+  // TODO: If these fail, still have to make sure to close the cluster
+  require(session.execute(s"SELECT * FROM $keyspace.$users").getColumnDefinitions.contains(Key))
+  require(session.execute(s"SELECT * FROM $keyspace.$users").getColumnDefinitions.contains(Value))
+  require(session.execute(s"SELECT * FROM $keyspace.$items").getColumnDefinitions.contains(Key))
+  require(session.execute(s"SELECT * FROM $keyspace.$items").getColumnDefinitions.contains(Value))
+  require(session.execute(s"SELECT * FROM $keyspace.$ratings").getColumnDefinitions.contains(Key))
+  require(session.execute(s"SELECT * FROM $keyspace.$ratings").getColumnDefinitions.contains(Value))
 
   def getFeatureData(itemId: Long): Try[FeatureVector] = {
     try {
-      val rawBytes = session.execute("SELECT * FROM " + itemsCF + " WHERE " + Key + " = " + itemId).all().get(0).getBytes(Value)
+      val rawBytes = session.execute(s"SELECT * FROM $keyspace.$items WHERE $Key = $itemId")
+          .all().get(0).getBytes(Value)
       val kryo = KryoThreadLocal.kryoTL.get
       val array = kryo.deserialize(rawBytes).asInstanceOf[FeatureVector]
       Success(array)
@@ -48,7 +50,8 @@ class CassandraStorage ( address: String,
 
   def getUserFactors(userId: Long): Try[WeightVector] = {
     try {
-      val rawBytes = session.execute("SELECT * FROM " + usersCF + " WHERE " + Key + " = " + userId).all().get(0).getBytes(Value)
+      val rawBytes = session.execute(s"SELECT * FROM $keyspace.$users WHERE $Key = $userId")
+          .all().get(0).getBytes(Value)
       val kryo = KryoThreadLocal.kryoTL.get
       val result = kryo.deserialize(rawBytes).asInstanceOf[WeightVector]
       Success(result)
@@ -59,7 +62,8 @@ class CassandraStorage ( address: String,
 
   def getAllObservations(userId: Long): Try[Map[Long, Double]] = {
     try {
-      val rawBytes = session.execute("SELECT * FROM " + ratingsCF + " WHERE " + Key + " = " + userId).all().get(0).getBytes(Value)
+      val rawBytes = session.execute(s"SELECT * FROM $keyspace.$ratings WHERE $Key = $userId")
+          .all().get(0).getBytes(Value)
       val kryo = KryoThreadLocal.kryoTL.get
       val result = kryo.deserialize(rawBytes).asInstanceOf[HashMap[Long, Double]]
       Success(result)
--- veloxms-core/src/main/scala/edu/berkeley/veloxms/storage/TachyonStorage.scala
+++ veloxms-core/src/main/scala/edu/berkeley/veloxms/storage/TachyonStorage.scala
@@ -21,89 +21,108 @@ import edu.berkeley.veloxms.util.{VeloxKryoRegistrar, KryoThreadLocal}
 
 // class TachyonStorage[U: ClassTag] (
 class TachyonStorage (
-    users: ClientStore,
-    items: ClientStore,
-    ratings: ClientStore,
-    // val numFactors: Int) extends ModelStorage[U] with LazyLogging {
-    val numFactors: Int) extends ModelStorage[FeatureVector] with Logging {
+                         tachyonMaster: String,
+                         userStoreName: String,
+                         itemStoreName: String,
+                         ratingsStoreName: String,
+                         // val numFactors: Int) extends ModelStorage[U] with LazyLogging {
+                         val numFactors: Int) extends ModelStorage[FeatureVector] with Logging {
 
-    // val logger = Logger(LoggerFactory.getLogger(classOf[TachyonStorage]))
+  val users = TachyonUtils.getStore(tachyonMaster, userStoreName) match {
+    case Success(s) => s
+    case Failure(f) => throw new RuntimeException(
+      s"Couldn't initialize use model: ${f.getMessage}")
+  }
 
-    def getFeatureData(itemId: Long): Try[FeatureVector] = {
-        // getFactors(itemId, items, "item-model")
-        // for {
-        //     rawBytes <- Try(items.get(TachyonUtils.long2ByteArr(userId)))
-        //     array <- Try(SerializationUtils.deserialize(rawBytes))
-        //     result <- Try(array match
-        //         case Failure(u) => array
-        //         case Success(u) => u.asInstanceOf[U]
-        //     )
-        // } yield result
+  val items = TachyonUtils.getStore(tachyonMaster, itemStoreName) match {
+    case Success(s) => s
+    case Failure(f) => throw new RuntimeException(
+      s"Couldn't initialize item model: ${f.getMessage}")
+  }
 
-        try {
-          val rawBytes = ByteBuffer.wrap(items.get(TachyonUtils.long2ByteArr(itemId)))
-          val kryo = KryoThreadLocal.kryoTL.get
-          val array = kryo.deserialize(rawBytes).asInstanceOf[FeatureVector]
-          Success(array)
-        } catch {
-          case u: Throwable => Failure(u)
-        }
-        //
-        // val (raw, result) = for {
-        //     rawBytes <- Try(items.get(TachyonUtils.long2ByteArr(itemId)))
-        //     array <- Try(SerializationUtils.deserialize(rawBytes))
-        // } yield (rawBytes, array)
-        // logger.info(s"Found item $raw")
-        // result match {
-        //     case Success(u) => Success(u.asInstanceOf[U])
-        //     case Failure(u) => result
-        // }
-        //
-    }
+  val ratings = TachyonUtils.getStore(tachyonMaster, ratingsStoreName) match {
+    case Success(s) => s
+    case Failure(f) => throw new RuntimeException(
+      s"Couldn't initialize use model: ${f.getMessage}")
+  }
 
-    def getUserFactors(userId: Long): Try[WeightVector] = {
-      val rawBytes = ByteBuffer.wrap(users.get(TachyonUtils.long2ByteArr(userId)))
+  logInfo("got tachyon stores")
+
+  def getFeatureData(itemId: Long): Try[FeatureVector] = {
+    // getFactors(itemId, items, "item-model")
+    // for {
+    //     rawBytes <- Try(items.get(TachyonUtils.long2ByteArr(userId)))
+    //     array <- Try(SerializationUtils.deserialize(rawBytes))
+    //     result <- Try(array match
+    //         case Failure(u) => array
+    //         case Success(u) => u.asInstanceOf[U]
+    //     )
+    // } yield result
+
+    try {
+      val rawBytes = ByteBuffer.wrap(items.get(TachyonUtils.long2ByteArr(itemId)))
       val kryo = KryoThreadLocal.kryoTL.get
-      val result = kryo.deserialize(rawBytes).asInstanceOf[WeightVector]
-      Success(result)
-
-
-        //
-        // val result = for {
-        //     rawBytes <- Try(users.get(TachyonUtils.long2ByteArr(userId)))
-        //     array <- Try(rawBytes.unpickle[WeightVector])
-        // } yield array
-        // result
+      val array = kryo.deserialize(rawBytes).asInstanceOf[FeatureVector]
+      Success(array)
+    } catch {
+      case u: Throwable => Failure(u)
     }
-
-    def getAllObservations(userId: Long): Try[Map[Long, Double]] = {
-
-      val rawBytes = ByteBuffer.wrap(ratings.get(TachyonUtils.long2ByteArr(userId)))
-      val kryo = KryoThreadLocal.kryoTL.get
-      val result = kryo.deserialize(rawBytes).asInstanceOf[HashMap[Long, Double]]
-      Success(result)
-        //
-        // val result = for {
-        //     rawBytes <- Try(ratings.get(TachyonUtils.long2ByteArr(userId)))
-        //     array <- Try(rawBytes.unpickle[Map[Long, Double]])
-        // } yield array
-        // result
-    }
-
-    // def cast(value: Any): Option[A] = {
-    //   val erasure = manifest[A] match {
-    //     case Manifest.Byte => classOf[java.lang.Byte]
-    //     case Manifest.Short => classOf[java.lang.Short]
-    //     case Manifest.Char => classOf[java.lang.Character]
-    //     case Manifest.Long => classOf[java.lang.Long]
-    //     case Manifest.Float => classOf[java.lang.Float]
-    //     case Manifest.Double => classOf[java.lang.Double]
-    //     case Manifest.Boolean => classOf[java.lang.Boolean]
-    //     case Manifest.Int => classOf[java.lang.Integer]
-    //     case m => m.erasure
-    //   }
-    //   if(erasure.isInstance(value)) Some(value.asInstanceOf[A]) else None
+    //
+    // val (raw, result) = for {
+    //     rawBytes <- Try(items.get(TachyonUtils.long2ByteArr(itemId)))
+    //     array <- Try(SerializationUtils.deserialize(rawBytes))
+    // } yield (rawBytes, array)
+    // logger.info(s"Found item $raw")
+    // result match {
+    //     case Success(u) => Success(u.asInstanceOf[U])
+    //     case Failure(u) => result
     // }
+    //
+  }
+
+  def getUserFactors(userId: Long): Try[WeightVector] = {
+    val rawBytes = ByteBuffer.wrap(users.get(TachyonUtils.long2ByteArr(userId)))
+    val kryo = KryoThreadLocal.kryoTL.get
+    val result = kryo.deserialize(rawBytes).asInstanceOf[WeightVector]
+    Success(result)
+
+
+    //
+    // val result = for {
+    //     rawBytes <- Try(users.get(TachyonUtils.long2ByteArr(userId)))
+    //     array <- Try(rawBytes.unpickle[WeightVector])
+    // } yield array
+    // result
+  }
+
+  def getAllObservations(userId: Long): Try[Map[Long, Double]] = {
+
+    val rawBytes = ByteBuffer.wrap(ratings.get(TachyonUtils.long2ByteArr(userId)))
+    val kryo = KryoThreadLocal.kryoTL.get
+    val result = kryo.deserialize(rawBytes).asInstanceOf[HashMap[Long, Double]]
+    Success(result)
+      //
+      // val result = for {
+      //     rawBytes <- Try(ratings.get(TachyonUtils.long2ByteArr(userId)))
+      //     array <- Try(rawBytes.unpickle[Map[Long, Double]])
+      // } yield array
+      // result
+  }
+
+  // def cast(value: Any): Option[A] = {
+  //   val erasure = manifest[A] match {
+  //     case Manifest.Byte => classOf[java.lang.Byte]
+  //     case Manifest.Short => classOf[java.lang.Short]
+  //     case Manifest.Char => classOf[java.lang.Character]
+  //     case Manifest.Long => classOf[java.lang.Long]
+  //     case Manifest.Float => classOf[java.lang.Float]
+  //     case Manifest.Double => classOf[java.lang.Double]
+  //     case Manifest.Boolean => classOf[java.lang.Boolean]
+  //     case Manifest.Int => classOf[java.lang.Integer]
+  //     case m => m.erasure
+  //   }
+  //   if(erasure.isInstance(value)) Some(value.asInstanceOf[A]) else None
+  // }
 
   /**
    * Cleans up any necessary resources
@@ -115,85 +134,85 @@ class TachyonStorage (
 
 object TachyonUtils {
 
-    def long2ByteArr(id: Long): Array[Byte] = {
-        // val key = ByteBuffer.allocate(8)
-        // key.putLong(id).array()
+  def long2ByteArr(id: Long): Array[Byte] = {
+    // val key = ByteBuffer.allocate(8)
+    // key.putLong(id).array()
 
-      val buffer = ByteBuffer.allocate(12)
-      val kryo = KryoThreadLocal.kryoTL.get
-      val result = kryo.serialize(id, buffer).array
-      result
+    val buffer = ByteBuffer.allocate(12)
+    val kryo = KryoThreadLocal.kryoTL.get
+    val result = kryo.serialize(id, buffer).array
+    result
+  }
+
+  // could make this a z-curve key instead
+  def twoDimensionKey(key1: Long, key2: Long): Array[Byte] = {
+    val key = ByteBuffer.allocate(16)
+    key.putLong(key1)
+    key.putLong(key2)
+    key.array()
+  }
+
+  def getStore(tachyon: String, kvloc: String): Try[ClientStore] = {
+    val url = s"$tachyon/$kvloc"
+    Try(ClientStore.getStore(new TachyonURI(url))) match {
+      case f: Failure[ClientStore] => f
+      case Success(u) => if (u == null) {
+        Failure(new RuntimeException(s"Tachyon store $url not found"))
+      } else {
+        Success(u)
+      }
     }
+  }
 
-    // could make this a z-curve key instead
-    def twoDimensionKey(key1: Long, key2: Long): Array[Byte] = {
-        val key = ByteBuffer.allocate(16)
-        key.putLong(key1)
-        key.putLong(key2)
-        key.array()
-    }
-
-    def getStore(tachyon: String, kvloc: String): Try[ClientStore] = {
-        val url = s"$tachyon/$kvloc"
-        Try(ClientStore.getStore(new TachyonURI(url))) match {
-            case f: Failure[ClientStore] => f
-            case Success(u) => if (u == null) {
-                Failure(new RuntimeException(s"Tachyon store $url not found"))
-            } else {
-                Success(u)
-            }
-        }
-    }
-
-    // def serializeArray(arr: Array[Double]): Array[Byte] = {
-    //   val buffer = ByteBuffer.allocate
-    // }
+  // def serializeArray(arr: Array[Double]): Array[Byte] = {
+  //   val buffer = ByteBuffer.allocate
+  // }
 
 
-    /*
-    Message bytes are:
-    8 bytes: request ID; high-order bit is request or response boolean
-    N bytes: serialized message
-     */
-    // def serializeMessage(requestId: RequestId, msg: Any, isRequest: Boolean): ByteBuffer = {
-    //   val buffer = ByteBuffer.allocate(4096)
-    //   var header = requestId & ~(1L << 63)
-    //   if(isRequest) header |= (1L << 63)
-    //   buffer.putLong(header)
-    //   //val kryo = VeloxKryoRegistrar.getKryo()
-    //   val kryo = KryoThreadLocal.kryoTL.get
-    //   val result = kryo.serialize(msg,buffer)
-    //   //VeloxKryoRegistrar.returnKryo(kryo)
-    //   result.flip
-    //   result
-    // }
+  /*
+  Message bytes are:
+  8 bytes: request ID; high-order bit is request or response boolean
+  N bytes: serialized message
+   */
+  // def serializeMessage(requestId: RequestId, msg: Any, isRequest: Boolean): ByteBuffer = {
+  //   val buffer = ByteBuffer.allocate(4096)
+  //   var header = requestId & ~(1L << 63)
+  //   if(isRequest) header |= (1L << 63)
+  //   buffer.putLong(header)
+  //   //val kryo = VeloxKryoRegistrar.getKryo()
+  //   val kryo = KryoThreadLocal.kryoTL.get
+  //   val result = kryo.serialize(msg,buffer)
+  //   //VeloxKryoRegistrar.returnKryo(kryo)
+  //   result.flip
+  //   result
+  // }
 
-    // def deserializeMessage(bytes: ByteBuffer): (Any, RequestId, Boolean) = {
-    //   val headerLong = bytes.getLong()
-    //   val isRequest = (headerLong >>> 63) == 1
-    //   val requestId = headerLong & ~(1L << 63)
-    //   // TODO: use Kryo serializer pool instead
-    //   //val kryo = VeloxKryoRegistrar.getKryo()
-    //   val kryo = KryoThreadLocal.kryoTL.get
-    //   val msg = kryo.deserialize(bytes)
-    //   //VeloxKryoRegistrar.returnKryo(kryo)
-    //   (msg, requestId, isRequest)
-    // }
+  // def deserializeMessage(bytes: ByteBuffer): (Any, RequestId, Boolean) = {
+  //   val headerLong = bytes.getLong()
+  //   val isRequest = (headerLong >>> 63) == 1
+  //   val requestId = headerLong & ~(1L << 63)
+  //   // TODO: use Kryo serializer pool instead
+  //   //val kryo = VeloxKryoRegistrar.getKryo()
+  //   val kryo = KryoThreadLocal.kryoTL.get
+  //   val msg = kryo.deserialize(bytes)
+  //   //VeloxKryoRegistrar.returnKryo(kryo)
+  //   (msg, requestId, isRequest)
+  // }
 
-    // def cast[A : Manifest](value: Any): Option[A] = {
-    //   val erasure = manifest[A] match {
-    //     case Manifest.Byte => classOf[java.lang.Byte]
-    //     case Manifest.Short => classOf[java.lang.Short]
-    //     case Manifest.Char => classOf[java.lang.Character]
-    //     case Manifest.Long => classOf[java.lang.Long]
-    //     case Manifest.Float => classOf[java.lang.Float]
-    //     case Manifest.Double => classOf[java.lang.Double]
-    //     case Manifest.Boolean => classOf[java.lang.Boolean]
-    //     case Manifest.Int => classOf[java.lang.Integer]
-    //     case m => m.erasure
-    //   }
-    //   if(erasure.isInstance(value)) Some(value.asInstanceOf[A]) else None
-    // }
+  // def cast[A : Manifest](value: Any): Option[A] = {
+  //   val erasure = manifest[A] match {
+  //     case Manifest.Byte => classOf[java.lang.Byte]
+  //     case Manifest.Short => classOf[java.lang.Short]
+  //     case Manifest.Char => classOf[java.lang.Character]
+  //     case Manifest.Long => classOf[java.lang.Long]
+  //     case Manifest.Float => classOf[java.lang.Float]
+  //     case Manifest.Double => classOf[java.lang.Double]
+  //     case Manifest.Boolean => classOf[java.lang.Boolean]
+  //     case Manifest.Int => classOf[java.lang.Integer]
+  //     case m => m.erasure
+  //   }
+  //   if(erasure.isInstance(value)) Some(value.asInstanceOf[A]) else None
+  // }
 
 
 }
