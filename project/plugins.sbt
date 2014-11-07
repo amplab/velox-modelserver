4b562204fb0514f9546d9845f1e96a33447f53cb
Dan Crankshaw <dscrankshaw@gmail.com>
2014-11-07 08:49:36 -0800

    Deleted old java files

--- project/plugins.sbt
+++
@@ -1,8 +0,0 @@
-
-// addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "2.2.0")
-
-addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.5.2")
-
-addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.10.2")
-
-
--- veloxms-writer/mat_predictions.sh
+++
@@ -1,5 +0,0 @@
-java -Xms17g -Xmx17g -Dlog4j.configuration=file:log4j.properties \
-  -jar target/velox-model-writer-0.0.1-SNAPSHOT.jar \
-  matPredictions \
-  ~/ml-10mil-data/whole-model/user_models/10M-50factors-all.txt \
-  ~/ml-10mil-data/whole-model/item_models/10M-50factors-all.txt
